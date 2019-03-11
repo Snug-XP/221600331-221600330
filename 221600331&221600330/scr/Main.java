3e6781f2b50f17e3e7029a6fb46dd9928f99bcc9
向鹏 <1055386758@qq.com>
2019-03-12 00:54:17 +0800

    Add files via upload

--- 221600331&221600330/scr/Main.java
+++ 221600331&221600330/scr/Main.java
@@ -1,1 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class Main {
+
+	public static void main(String[] args) {
+		readTxtFile("input.txt");
+
+	}
+	public static void readTxtFile(String filePath){
+        try {
+                File input_file=new File(filePath);
+                File output_file=new File("result.txt");
+                if(input_file.isFile() && input_file.exists()){ //判断文件是否存在
+                    InputStreamReader read = new InputStreamReader(new FileInputStream(input_file));
+                    OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(output_file));
+                    BufferedReader bufferedReader = new BufferedReader(read);
+                    BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                    String text = null;
+                    int sum = 0;
+                    while((text = bufferedReader.readLine()) != null){
+                    	text = text.toLowerCase();
+                    	text.replaceAll("\r\n", "\n");
+                        System.out.println(text+"  长度:"+ (text.length()) );
+                        sum+=text.length();
+                        bufferedWriter.write(text+"\r\n");
+                        bufferedWriter.flush();
+                    }
+                    System.out.println("sum="+sum);
+                   
+                    
+                    read.close();
+                    writer.close();
+                 }
+                 else{
+                	 System.out.println("找不到指定的文件");
+                 }
+        } catch (Exception e) {
+            System.out.println("读取文件内容出错");
+            e.printStackTrace();
+        }
+     
+    }
+
+
+}
---
+++ 221600331&221600330/scr/wordCount.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class wordCount {
+	String str;
+	wordCount(String str)
+	{
+		this.str = str;
+	}
+	
+	
+	
+	
+}
